0d7e82dca40d004f793f07630d91827d0a78ece0
Jose Balado <JoseBalado@users.noreply.github.com>
2022-05-01 14:16:07 +0200

    Creating a folder for testing files

--- Program.cs
+++ Program.cs
@@ -113,7 +113,7 @@ public class Example
         try
         {
             // Open the text file using a stream reader.
-            using (var sr = new StreamReader(fileName))
+            using (var sr = new StreamReader($"text_files{Path.DirectorySeparatorChar}{fileName}"))
             {
                 // Read the stream as a string, and write the string to the console.
                 return sr.ReadToEnd().ToString();
